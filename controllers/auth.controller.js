fb36c43c0866a938e230b7f38fa39e8ebb0bfb3e
César Robles <robles.cesar@proton.me>
2025-11-10 14:37:32 -0300

    fix: varios

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -55,28 +55,26 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: 'Usuario o correo ya existe' });
     }
 
-    // 2. Hashear la contraseña
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // 3. Crear el nuevo usuario
-    // Para esta puerta trasera, usamos el 'loginIdentifier' para ambos campos
+    // 2. Crear el nuevo usuario
+    // (Se elimina la encriptación manual. El modelo .pre('save') lo hará)
+    
     const nuevoUsuario = new Usuario({
       nombre: loginIdentifier,
       correo: loginIdentifier,
-      password: hashedPassword
-      // El rol se asignará según el 'default' de tu modelo
+      password: password, // <-- Pasamos la clave en texto plano
+      rut: '1-1'          // <-- Añadimos el RUT de relleno para pasar la validación
+      // El 'rol' (Admin), 'cargo' (Estudiante) y 'situacion' (Vigente)
+      // se asignarán por 'default' gracias a tu modelo.
     });
 
-    // 4. Guardar en la base de datos
+    // 3. Guardar en la base de datos (aquí se activa el .pre('save') y encripta)
     await nuevoUsuario.save();
 
-    // 5. Enviar respuesta de éxito
-    // No devolvemos un token, forzamos a que inicie sesión después
+    // 4. Enviar respuesta de éxito
     res.status(201).json({ message: 'Usuario registrado con éxito' });
 
   } catch (error) {
-    // AÑADE ESTA LÍNEA
+    // Esto imprime el error en los logs de Render
     console.error('Error en /register:', error); 
     
     res.status(500).json({ message: 'Error en el servidor', error: error.message });
